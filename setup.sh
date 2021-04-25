29223be5bd737664c6ef88ef5325c354ea17e6ec
Thad Craft <thad.craft@kohls.com>
2021-04-25 14:49:23 -0400

    Updates

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #! /bin/bash
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew install wget zsh zsh-completions zsh-git-prompt
 brew install kube-ps1
 brew install kubernetes-cli kubectx kubernetes-helm kubeseal
-brew install romkatv/powerlevel10k/powerlevel10k
 brew install rbenv
 brew install direnv
 brew install cloudfoundry/tap/cf-cli@7
@@ -19,6 +18,8 @@ brew install k9s
 brew install python3
 brew install git
 brew install hashicorp/tap/vault
-brew install jenkins-x/jx/jx
 brew tap homebrew/cask-fonts
 brew install docker font-awesome-terminal-fonts
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+

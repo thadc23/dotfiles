e7357e8844c0689d9bbfda8e5ff06c6ca4243a4b
Thad Craft <tcraft@Thads-Mac-mini.local>
2023-05-08 16:40:44 -0400

    cleanup scripts

--- scripts/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "brew update"
-brew update
-echo "brew upgrade"
-brew upgrade
-echo "Done!"
-
--- scripts/vote.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while true
-do  
-  curl -s -X POST https://retros-iad-api.cfapps.io/retros/customer0/items/$1/vote &
-done
